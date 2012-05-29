356570e8893a46a12e0fe792a24ace7d1b07b4a3
Yang Sheng Han <shenghan.yang@gmail.com>
2012-05-29 12:11:53 +0800

    [ADD] Adds a global layout and index layout.

--- application/components/Controller.php
+++ application/components/Controller.php
@@ -10,14 +10,4 @@ class Controller extends CController
      * @var string the default layout for the controller view.
      */
     public $layout = '//layouts/main';
-
-    /**
-     * @var array context menu items.
-     */
-    public $menu = array();
-
-    /**
-     * @var array the breadcrumbs of the current page.
-     */
-    public $breadcrumbs = array();
 }
