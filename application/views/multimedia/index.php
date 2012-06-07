91db06cc036d4d51d0967c2acad8690cbadf7b47
DiMo <a968574123@hotmail.com>
2012-06-08 03:04:32 +0800

    [MOD] VIEW/MULTIMEDIA

--- application/views/multimedia/index.php
+++ application/views/multimedia/index.php
@@ -1,2 +1,12 @@
 <?php $this->pageTitle = '影音專區'; ?>
+<div id="mm_container">
 影音專區
+<div id="mm_sidebar">
+<div id="mm_list"></div>
+<div id="mm_plarer"></div>
+</div>
+<div id="mm_content">
+<div id="mm_video"></div>
+<div id="mm_introduction"></div>
+</div>
+</div>
