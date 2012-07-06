d7075660afb8acca45b3c1cf75e38dbcd96d3e3c
Sheng Han Yang <shenghan.yang@gmail.com>
2012-07-07 02:52:34 +0800

    [FIX] Fixes a bug in Security component.  [ENH] Improves style of form elements.

--- application/components/Security.php
+++ application/components/Security.php
@@ -23,7 +23,7 @@ class Security extends CApplicationComponent
         {
             $token = Yii::app()->session['token'];
             unset(Yii::app()->session['token']);
-            if ( $token !== $_POST['token'] )
+            if ( ! isset($_POST['token']) || $token !== $_POST['token'] )
             {
                 if ( YII_DEBUG )
                 {
