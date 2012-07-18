6722add0bdfbd72ffe6f96e4f252da59cdf5cdd5
Liang <betty171920@gmail.com>
2012-07-18 21:59:11 +0800

    系所連結、club model完成一點點

---
+++ application/controllers/ClubController.php
@@ -0,0 +1,35 @@
+<?php 
+
+class ClubController extends Controller
+{
+	public function actionIndex()
+	{
+        $this->render('index');
+        
+	}
+
+	public function actionClub()
+	{
+		$this->render('club');
+	}
+	
+	public function actionStudent()
+	{
+		$this->render('student');
+	}
+	
+	public function actionDepartment()
+	{
+		$this->render('department');
+	
+	}
+    public function actionContent()
+    {
+        $club = new Club;
+        $data = $club->getClub(1);
+    
+        $this->render('content',array('data'=>$data));
+       
+    }
+}
+?>
