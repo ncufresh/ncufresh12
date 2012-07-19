fb10a0d875551715f8bb45c9a01f599119dd9d53
POPO <girl820628@hotmail.com>
2012-07-19 10:58:29 +0800

    modify the table and page about friends

--- application/controllers/FriendsController.php
+++ application/controllers/FriendsController.php
@@ -12,11 +12,11 @@ class FriendsController extends Controller
             $id = (integer)$_GET['id'];
             if ( $id === 1 ) // 同系同屆 
             {
-                $this->redirect(array('friends/samedepartment_samegrade'));
+                $this->redirect(array('friends/samedepartmentsamegrade'));
             }
             else if ( $id === 2 ) // 同系不同屆
             {
-                $this->redirect(array('friends/samedepartment_diffgrade'));
+                $this->redirect(array('friends/samedepartmentdiffgrade'));
             }
             else if ($id === 3 ) // 其它科系
             {
@@ -24,5 +24,21 @@ class FriendsController extends Controller
             }
         }
         $this->render('friends');
+    }
+    
+    public function actionSameDepartmentSameGrade() // 好友專區
+    {
+         $this->setPageTitle(Yii::app()->name . ' - 同系同屆');
+         $this->render('samedepartmentsamegrade');
+    }
+    public function actionSameDepartmentDiffGrade() // 好友專區
+    {
+        $this->setPageTitle(Yii::app()->name . ' - 同系不同屆');
+        $this->render('samedepartmentdiffgrade');
+    }
+    public function actionOtherDepartment() // 好友專區
+    {
+         $this->setPageTitle(Yii::app()->name . ' - 其他科系');
+         $this->render('otherdepartment');
     }
 }
