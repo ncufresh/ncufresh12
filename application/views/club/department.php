2ea38702fe75ac5b2db29a74ab29ed8693248c21
Liang <betty171920@gmail.com>
2012-07-19 20:01:29 +0800

    社團id、連結，社團資料庫完成

--- application/views/club/department.php
+++ application/views/club/department.php
@@ -1,4 +1,9 @@
-<div id="club-all">
+<div>
+	<div id="club">
+        <a href="<?php echo Yii::app()->createUrl('club/club');?>" title="社團">社團</a>
+        <a href="<?php echo Yii::app()->createUrl('club/student');?>" title="學生組織">學生組織</a>
+        <a href="<?php echo Yii::app()->createUrl('club/department');?>" title="系所">系所</a>
+    </div>
     <div id="content">
         <ul class="l1">
             <h2>資電學院</h2>
@@ -24,34 +29,62 @@
                 <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="機械系">機械系</a>
             </li>
         </ul>
-        <div class="l1">
+        <ul class="l1">
             <h1>理學院</h1>
+			<li>
             <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="生科系">生科系</a>
-            <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="物理系">物理系</a>
-            <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="化學系">化學系</a>
-            <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="數學系">數學系</a>
-            <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="光電系">光電系</a>
-            <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="理學院學士班">理學院學士班</a>
-        </div>
-            <div class="l1">
+            </li>
+			<li>
+			<a href="<?php echo Yii::app()->createUrl('club/content');?>" title="物理系">物理系</a>
+            </li>
+			<li>
+			<a href="<?php echo Yii::app()->createUrl('club/content');?>" title="化學系">化學系</a>
+            </li>
+			<li>
+			<a href="<?php echo Yii::app()->createUrl('club/content');?>" title="數學系">數學系</a>
+            </li>
+			<li>
+			<a href="<?php echo Yii::app()->createUrl('club/content');?>" title="光電系">光電系</a>
+            </li>
+			<li>
+			<a href="<?php echo Yii::app()->createUrl('club/content');?>" title="理學院學士班">理學院學士班</a>
+			</li>
+		</ul>
+        <ul class="l1">
             <h1>文學院</h1>
+			<li>
             <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="中文系">中文系</a>
-            <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="英文系">英文系</a>
-            <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="法文系">法文系</a>
-        </div>
-        </div>
-            <div class="l1">
+            </li>
+			<li>
+			<a href="<?php echo Yii::app()->createUrl('club/content');?>" title="英文系">英文系</a>
+            </li>
+			<li>
+			<a href="<?php echo Yii::app()->createUrl('club/content');?>" title="法文系">法文系</a>
+			</li>
+		</ul>
+        <ul class="l1">
             <h2>管理學院</h2>
+			<li>
             <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="資管系">資管系</a>
-            <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="企管系">企管系</a>
-            <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="經濟系">經濟系</a>
-            <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="財經系">財經系</a>
-        </div>
-        </div>
-            <div class="l1">
+            </li>
+			<li>
+			<a href="<?php echo Yii::app()->createUrl('club/content');?>" title="企管系">企管系</a>
+            </li>
+			<li>
+			<a href="<?php echo Yii::app()->createUrl('club/content');?>" title="經濟系">經濟系</a>
+            </li>
+			<li>
+			<a href="<?php echo Yii::app()->createUrl('club/content');?>" title="財經系">財經系</a>
+			</li>
+		</ul>
+        <ul class="l1">
             <h2>地科學院</h2>
+			<li>
             <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="大氣系">大氣系</a>
-            <a href="<?php echo Yii::app()->createUrl('club/content');?>" title="地科系">地科系</a>
-        </div>
+            </li>
+			<li>
+			<a href="<?php echo Yii::app()->createUrl('club/content');?>" title="地科系">地科系</a>
+			</li>
+		</ul>
     </div>
 </div>
