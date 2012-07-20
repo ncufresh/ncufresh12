0d2bf6766e5fcd0906c536c374eacc067f62b109
Sheng Han Yang <shenghan.yang@gmail.com>
2012-07-20 08:39:02 +0800

    [NEW] Adds new Yii console tools.

---
+++ application/yiic.php
@@ -0,0 +1,27 @@
+<?php
+
+defined('NCUFRESH2012') or define('NCUFRESH2012', true);
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+
+$yii = require_once('../ncufresh2012.php');
+
+$yiic = dirname($yii) . DIRECTORY_SEPARATOR . 'yiic.php';
+
+$config = array(
+    'basePath'  => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'application',
+	'components'=> array(
+        'db'            => array(
+            'connectionString'  => 'mysql:host=' . $ncufreshdb['host'] . ';dbname=' . $ncufreshdb['database'],
+            'emulatePrepare'    => true,
+            'username'          => $ncufreshdb['username'],
+            'password'          => $ncufreshdb['password'],
+            'charset'           => 'utf8',
+            'tablePrefix'       => ''
+        )
+	)
+);
+
+require_once($yiic);
