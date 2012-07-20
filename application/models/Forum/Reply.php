a100d6b3193becf1b4570da969f3ea4628988560
cyrandy <cyrandy@gmail.com>
2012-07-20 21:12:44 +0800

    add reply function

--- application/models/Forum/Reply.php
+++ application/models/Forum/Reply.php
@@ -1,6 +1,6 @@
 <?php
 
-class ForumReply extends CActiveRecord
+class Reply extends CActiveRecord
 {
     public static function model($className = __CLASS__)
     {
@@ -10,5 +10,26 @@ class ForumReply extends CActiveRecord
     public function tableName()
     {
         return '{{forum_replies}}';
+    }
+    
+    public function relations(){
+        return array('article'=>array(self::BELONGS_TO,'Article','article_id'));
+    }
+    
+    public function beforeSave()
+    {
+        //尚未檢查是否所有資料都有填寫
+        if ( parent::beforeSave() )
+        {
+            if ( $this->isNewRecord )
+            {
+                // 如果未登入author_id=0 ; 檢查登入與否
+                $this->author_id = Yii::app()->user->getId();
+                $this->create_time = TIMESTAMP;
+            }
+            //$this->update_time = TIMESTAMP;
+            return true;
+        }
+        return false;
     }
 }
