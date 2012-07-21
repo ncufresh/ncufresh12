7840ca9b412ab2ae64ba8de83f1366216e12289d
sin <xxice520@hotmail.com>
2012-07-21 21:22:30 +0800

    [New] nculife.js [Update] nculife index-list [Fix] nculifeController

--- application/controllers/NculifeController.php
+++ application/controllers/NculifeController.php
@@ -12,15 +12,27 @@ class NculifeController extends Controller
     {
         $this->render('live');
     }
+    public function actionLiveContent($tab_id)
+    {
+        switch ( $tab_id )
+        {
+            case 1 : 
+                $this->_data['content'] = $this->renderPartial('livecontent/1', null, true, false);
+                break;
+            case 2 :
+                $this->_data['content'] = $this->renderPartial('livecontent/2', null, true, false);
+                break;
+        }
+    }
 
     public function actionFood()
     {
         $this->render('food');
     }
 
-    public function actionFoodContent($id)
+    public function actionFoodContent($tab_id)
     {
-        switch ( $id )
+        switch ( $tab_id )
         {
             case 1 : 
                 $this->_data['content'] = $this->renderPartial('foodcontent/1', null, true, false);
@@ -35,11 +47,35 @@ class NculifeController extends Controller
     {
         $this->render('house');
     }
+    public function actionHouseContent($tab_id)
+    {
+        switch ( $tab_id )
+        {
+            case 1 : 
+                $this->_data['content'] = $this->renderPartial('housecontent/1', null, true, false);
+                break;
+            case 2 :
+                $this->_data['content'] = $this->renderPartial('housecontent/2', null, true, false);
+                break;
+        }
+    }
 
     public function actionCar()
     {
          $this->render('car');
         /* $this->renderPartial('car'); */
+    }
+    public function actionCarContent($tab_id)
+    {
+        switch ( $tab_id )
+        {
+            case 1 : 
+                $this->_data['content'] = $this->renderPartial('carcontent/1', null, true, false);
+                break;
+            case 2 :
+                $this->_data['content'] = $this->renderPartial('carcontent/2', null, true, false);
+                break;
+        }
     }
 
     public function actionOutside()
@@ -47,13 +83,50 @@ class NculifeController extends Controller
         $this->render('outside');
     }
 
+    public function actionOutsideContent($tab_id)
+    {
+        switch ( $tab_id )
+        {
+            case 1 : 
+                $this->_data['content'] = $this->renderPartial('outsidecontent/1', null, true, false);
+                break;
+            case 2 :
+                $this->_data['content'] = $this->renderPartial('outsidecontent/2', null, true, false);
+                break;
+        }
+    }
+
     public function actionSport()
     {
         $this->render('sport');
+    }
+    public function actionSportContent($tab_id)
+    {
+        switch ( $tab_id )
+        {
+            case 1 : 
+                $this->_data['content'] = $this->renderPartial('sportcontent/1', null, true, false);
+                break;
+            case 2 :
+                $this->_data['content'] = $this->renderPartial('sportcontent/2', null, true, false);
+                break;
+        }
     }
 
     public function actionHealth()
     {
         $this->render('health');
+    }
+    public function actionHealthContent($tab_id)
+    {
+        switch ( $tab_id )
+        {
+            case 1 : 
+                $this->_data['content'] = $this->renderPartial('healthcontent/1', null, true, false);
+                break;
+            case 2 :
+                $this->_data['content'] = $this->renderPartial('healthcontent/2', null, true, false);
+                break;
+        }
     }
 }
--- application/views/nculife/sport.php
+++ application/views/nculife/sport.php
@@ -1,1 +1,34 @@
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+            jQuery.configures.ncuLifeUrl = '<?php echo Yii::app()->createAbsoluteUrl('nculife/sportcontent',array('tab_id'=> ':id'));?>';
+    });
+</script>
 中大運動
+<div class="nculife-sport">
+    <button class="sport-dialog">游泳池</button>
+    <button class="sport-dialog">依仁堂</button>
+    <button class="sport-dialog">球類場地</button>
+    <button class="sport-dialog">溜冰場</button>
+    <button class="sport-dialog">操場</button>
+
+    <div id="nculife-dialog" title="Basic modal dialog">
+        <div id="nculife-dh">
+            <ul>
+                <li class="life-tab"><a href="#1" id="haha1">TAB1</a></li>
+                <li class="life-tab"><a href="#2" id="haha2">TAB2</a></li>
+                <li class="life-tab"><a href="#3" id="haha3">TAB3</a></li>
+            </ul>
+            
+            
+            
+        </div><!-- end nculife-dh -->
+        <div id="nculife-db">
+            <div id="nculife-cv">
+                View
+            </div><!-- end nculife-cv -->
+            <div id="nculife-ct">
+                Text
+            </div><!-- end nculife-ct -->
+        </div><!-- end nculife-db -->
+    </div><!-- end nculife-dialog -->
+</div><!-- end nculife-food --><!-- end -->
