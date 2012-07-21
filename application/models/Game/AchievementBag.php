826079e4e272c61a2ff185a760e30afd52ade2b5
Winston <win60615@yahoo.com.tw>
2012-07-21 20:28:50 +0800

    add new model

---
+++ application/models/Game/AchievementBag.php
@@ -0,0 +1,25 @@
+<?php
+
+class AchievementBag extends CActiveRecord
+{
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public function tableName()
+    {
+        return '{{game_achievements_bag}}';
+    }
+    
+    public function relations()
+    {
+        return array(
+            'translation'    => array(
+                self::BELONGS_TO,
+                'Achievement',
+                'achievement_id'
+            )
+        );
+    }
+}
