b6329dc46489bcb21730a451f02c0a4514a5d60e
Liang <betty171920@gmail.com>
2012-07-22 19:02:23 +0800

    社團資料、照片全部整理完，新增了管理介面(只是還很醜)、controller的新action還沒寫完

--- application/views/club/content.php
+++ application/views/club/content.php
@@ -66,37 +66,40 @@ jQuery(document).ready(function()
             <button>UP</button>
             <button>DOWN</button>
             <div id="club-menu-items">
-                <button class="club-picture1"><img src="<?php echo Yii::app()->baseUrl . "/statics/images/club/$id/1.jpg"?>"/></button>
-                <button class="club-picture2"><img src="<?php echo Yii::app()->baseUrl . "/statics/images/club/$id/2.jpg"?>"/></button>
-                <button class="club-picture3"><img src="<?php echo Yii::app()->baseUrl . "/statics/images/club/$id/3.jpg"?>"/></button>
-				<button class="club-picture4"><img src="<?php echo Yii::app()->baseUrl . "/statics/images/club/$id/4.jpg"?>"/></button>
+                <button class="club-picture1"><img src="<?php echo Yii::app()->baseUrl . "/files/club/$id/1.jpg"?>"/></button>
+                <button class="club-picture2"><img src="<?php echo Yii::app()->baseUrl . "/files/club/$id/2.jpg"?>"/></button>
+                <button class="club-picture3"><img src="<?php echo Yii::app()->baseUrl . "/files/club/$id/3.jpg"?>"/></button>
+				<button class="club-picture4"><img src="<?php echo Yii::app()->baseUrl . "/files/club/$id/4.jpg"?>"/></button>
 			</div>
 	</div>
 </div>
 <div id="club-picture-dialog1">
-	<div class="club-picture1-display1"><img src="<?php echo Yii::app()->baseUrl . "/statics/images/club/$id/1.jpg"?>"/></div>
+	<div class="club-picture1-display1"><img src="<?php echo Yii::app()->baseUrl . "/files/club/$id/1.jpg"?>"/></div>
 </div>
 <div id="club-picture-dialog2">
-	<div class="club-picture2-display2"><img src="<?php echo Yii::app()->baseUrl . "/statics/images/club/$id/2.jpg"?>"/></div>
+	<div class="club-picture2-display2"><img src="<?php echo Yii::app()->baseUrl . "/files/club/$id/2.jpg"?>"/></div>
 </div>
 <div id="club-picture-dialog3">
-	<div class="club-picture-display3"><img src="<?php echo Yii::app()->baseUrl . "/statics/images/club/$id/3.jpg"?>"/></div>
+	<div class="club-picture-display3"><img src="<?php echo Yii::app()->baseUrl . "/files/club/$id/3.jpg"?>"/></div>
 </div>
 <div id="club-picture-dialog4">
-	<div class="club-picture-display4"><img src="<?php echo Yii::app()->baseUrl . "/statics/images/club/$id/4.jpg"?>"/>"/></div>
+	<div class="club-picture-display4"><img src="<?php echo Yii::app()->baseUrl . "/files/club/$id/4.jpg"?>"/>"/></div>
 </div>
 
 <div id="club-display">    
-    <?php if((integer)$_GET['id']>=1&&(integer)$_GET['id']<=77)
+    <?php if(!$data->category)
             {?>
                 <a href="<?php echo Yii::app()->createUrl('club/club');?>" title="社團">社團</a>
                 <?php echo "→".$data->name?>
            <?}?>
-    <?php if((integer)$_GET['id']>77&&(integer)$_GET['id']<=98)
+    <?php if($data->category)
             {?>
                 <a href="<?php echo Yii::app()->createUrl('club/department');?>" title="系所">系所</a>
                 <?php echo "→".$data->name?>
            <?}?>
+	<?php
+    echo CHtml::link('修改', Yii::app()->createUrl('club/modify')) . '<br />';
+    ?>
     <h1><?php echo $data->name;?></h1>    
     <div id="club-title">簡介:</div>
     <br/>
