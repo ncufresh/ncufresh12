7847ad88934d8aeca423064d8d31df3782389a24
Sheng Han Yang <shenghan.yang@gmail.com>
2012-07-23 10:25:51 +0800

    [NEW] Adds shortcut icon for the website.  [ENH] Improves the performance of website by modifing .htaccess file.        If thw change on the page do not have effect, try press CTRL + F5 to        refresh all resource completely, that is, clear the caches.  [MOD] For the performance, the chat will not response a non-exists icon,        instead, it return an empty image.

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/statics/styles/ie.css" media="screen, projection" />
     <![endif]-->
 
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/statics/favicon.ico" rel="shortcut icon">
+
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js"></script>
