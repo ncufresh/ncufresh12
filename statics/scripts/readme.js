01f49f417b9b655faf185201cbbb36dfcd4ccec6
sin <xxice520@hotmail.com>
2012-07-23 09:26:20 +0800

    [Update] readme-menu

--- statics/scripts/readme.js
+++ statics/scripts/readme.js
@@ -1,3 +1,21 @@
 jQuery(document).ready(function()
 {
+    jQuery('#readme-direct').hover(function()
+    {
+        jQuery('#caption').animate(
+        {
+            left: '0px'
+        },500);
+    });
+    
+    jQuery('#readme-direct').mouseout(function()
+    {
+        jQuery('#caption').animate(
+        {
+            left: '-300px'
+        },500);
+    });
+    
+    
+    
 });
