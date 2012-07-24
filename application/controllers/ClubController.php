15ef9e05b9bb543b4ba26f2a4064ec44ffc7e8bf
Liang <betty171920@gmail.com>
2012-07-24 22:08:49 +0800

    一次可上傳四張圖片、uploadpicture介面修改

--- application/controllers/ClubController.php
+++ application/controllers/ClubController.php
@@ -72,87 +72,20 @@ class ClubController extends Controller
         /* $userID = Yii::app()->user->id;
         $model = $model->getClub($id); 
         $manager = findAll( $model->manager_id AND $userID );*/
-        $img_url = Yii::app()->baseUrl . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . "club/$id";
-        $name = " ";
-        $status1 = " ";
-        $status2 = " ";
-        $status3 = " ";
-        $status4 = " ";
-        $path = dirname(Yii::app()->basePath) . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . "club/$id";
-        if(isset($_POST['pic1']))
+        $path = dirname(Yii::app()->basePath) . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . 'club/' . $id;
+        if ( isset($_FILES['pictures']) )
         {
-            if(isset($_FILES['picture1']['name']))
+            for ( $index = 0 ; $index < 4 ; ++$index )
             {
-                $name = $_FILES['picture1']['name'];
-                $target = $path . DIRECTORY_SEPARATOR . $name;
-                if(file_exists("$path/1.jpg"))
-                {
-                    unlink("$path/1.jpg");
-                }
-                move_uploaded_file($_FILES['picture1']['tmp_name'], $target);
-                $temp = $_FILES['picture1']['tmp_name'];
-                rename($target,"$path/1.jpg");
-                $picture_size = $_FILES['picture1']['size'];
-                $picture_type = $_FILES['picture1']['type'];
-                $status1 = "檔案已上傳";
+                if ( empty($_FILES['pictures']['name'][$index]) ) continue;
+                $file = $path . DIRECTORY_SEPARATOR . ($index + 1) . '.jpg';
+                if ( file_exists($file) ) unlink($file);
+                move_uploaded_file($_FILES['pictures']['tmp_name'][$index], $file);
             }
-        }
-        if(isset($_POST['pic2']))
-        {
-            $name = $_FILES['picture2']['name'];
-            $target = $path . DIRECTORY_SEPARATOR . $name;
-            if(file_exists("$path/2.jpg"))
-            {
-                unlink("$path/2.jpg");
-            }
-            move_uploaded_file($_FILES['picture2']['tmp_name'], $target);
-            $temp = $_FILES['picture2']['tmp_name'];
-            rename($target,"$path/2.jpg");
-            $picture_size = $_FILES['picture2']['size'];
-            $picture_type = $_FILES['picture2']['type'];
-            $status2 = "檔案已上傳";
-        } 
-        if(isset($_POST['pic3']))
-        {
-            $name = $_FILES['picture3']['name'];
-            $target = $path . DIRECTORY_SEPARATOR . $name;
-            if(file_exists("$path/3.jpg"))
-            {
-                unlink("$path/3.jpg");
-            }
-            move_uploaded_file($_FILES['picture3']['tmp_name'], $target);
-            $temp = $_FILES['picture3']['tmp_name'];
-            rename($target,"$path/3.jpg");
-            $picture_size = $_FILES['picture3']['size'];
-            $picture_type = $_FILES['picture3']['type'];
-            $status3 = "檔案已上傳";
-        }
-        if(isset($_POST['pic4']))
-        {
-            $name = $_FILES['picture4']['name'];
-            $target = $path . DIRECTORY_SEPARATOR . $name;
-            if(file_exists("$path/4.jpg"))
-            {
-                unlink("$path/4.jpg");
-            }
-            move_uploaded_file($_FILES['picture4']['tmp_name'], $target);
-            $temp = $_FILES['picture4']['tmp_name'];
-            rename($target,"$path/4.jpg");
-            $picture_size = $_FILES['picture4']['size'];
-            $picture_type = $_FILES['picture4']['type'];
-            $status4 = "檔案已上傳";
-        }
-        if(isset($_POST['return']))
-        {
             $this->redirect(array("club/content/$id"));
         }
-        
         $this->render('uploadpicture',array(
             'id'=>$id,
-            'status1'=>$status1,
-            'status2'=>$status2,
-            'status3'=>$status3,
-            'status4'=>$status4
         ));
     } 
 }
--- application/views/club/uploadpicture.php
+++ application/views/club/uploadpicture.php
@@ -1,33 +1,9 @@
-<form method="post" enctype="multipart/form-data" action="<?php echo Yii::app()->createUrl('club/uploadpicture',array('id'=>$id))?>" >
-    <h3>上傳圖片1</h3>
-    <input type="file" style="width:600px;height:50px;" name="picture1"/>  <button name = "pic1" style="width:100px;height:30px;" type="submit">上傳</button>
-    <br/>
-    <?php echo $status1?>
-    <br/>
-</form>
-<form method="post" enctype="multipart/form-data" action="<?php echo Yii::app()->createUrl('club/uploadpicture',array('id'=>$id))?>" >
-    <h3>上傳圖片2</h3>
-    <input type="file" style="width:600px;height:50px;" name="picture2"/>  <button name = "pic2" style="width:100px;height:30px;" type="submit">上傳</button>
-    <br/>
-    <?php echo $status2?>
-    <br/>
-</form>
-<form method="post" enctype="multipart/form-data" action="<?php echo Yii::app()->createUrl('club/uploadpicture',array('id'=>$id))?>" >
-    <h3>上傳圖片3</h3>
-    <input type="file" style="width:600px;height:50px;" name="picture3"/>  <button name = "pic3" style="width:100px;height:30px;" type="submit">上傳</button>
-    <br/>
-    <?php echo $status3?>
-    <br/>
-</form>
-<form method="post" enctype="multipart/form-data" action="<?php echo Yii::app()->createUrl('club/uploadpicture',array('id'=>$id))?>" >
-    <h3>上傳圖片4</h3>
-    <input type="file" style="width:600px;height:50px;" name="picture4"/>  <button name = "pic4" style="width:100px;height:30px;" type="submit">上傳</button>
-    <br/>
-    <?php echo $status4?>
-    <br/>
-</form>
-<form method="post" enctype="multipart/form-data" action="<?php echo Yii::app()->createUrl('club/uploadpicture',array('id'=>$id))?>" >
-    <button type="submit" style="width:600px;height:50px;" name="return">上一頁</button>
-    <br/>
-    <br/>
+<form method="POST" enctype="multipart/form-data" action="<?php echo Yii::app()->createUrl('club/uploadpicture', array('id' => $id))?>" >
+    <h3>上傳圖片</h3>
+    <input type="file" name="pictures[0]" />
+    <input type="file" name="pictures[1]" />
+    <input type="file" name="pictures[2]" />
+    <input type="file" name="pictures[3]" />
+    <input name="token" value="<?php echo Yii::app()->security->getToken(); ?>" type="hidden" />
+    <button type="submit">上傳</button>
 </form>
