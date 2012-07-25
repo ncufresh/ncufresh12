b7200c711a3e10e83c7d9de91c869939fd824a27
Winston <win60615@yahoo.com.tw>
2012-07-25 21:52:31 +0800

    change controller.models.views

--- application/models/Game/Achievement.php
+++ application/models/Game/Achievement.php
@@ -22,4 +22,59 @@ class Achievement extends CActiveRecord
             )
         );
     }
+    
+    public function getAchievementsByUserId($id)
+    {
+        $user_value = array(
+            'login_times' => 24,
+            'spend_money' => 234324234,
+            'total_money' => 13333,
+            'friend' => 23,
+            'reply' => 10,
+            'post' => 15,
+            'cloth' => 15,
+            'throw_other_garbage' => 37,
+            'clean_self_garbage' => 32,
+            'other_throw_garbage' => 123,
+            'clean_other_garbage' => 21,
+            'body_price' => 51
+            );
+        $user_login_times = $user_value['login_times'];
+        $user_spend_money = $user_value['spend_money'];
+        $user_total_money = $user_value['total_money'];
+        $user_friend = $user_value['friend'];
+        $user_reply = $user_value['reply'];
+        $user_post = $user_value['post'];
+        $user_cloth = $user_value['cloth'];
+        $user_throw_other_garbage = $user_value['throw_other_garbage'];
+        $user_clean_self_garbage = $user_value['clean_self_garbage'];
+        $user_other_throw_garbage = $user_value['other_throw_garbage'];
+        $usre_clean_other_garbage = $user_value['clean_other_garbage'];
+        $user_body_price = $user_value['body_price'];
+        
+        // $character = Character::model()->findByPk($id);
+        $achievements = Achievement::model()->findAll();
+        $return = array();
+        foreach($achievements as $achievement)
+        {
+            if( $user_login_times >= $achievement->login_times &&
+                $user_spend_money >= $achievement->spend_money &&
+                $user_total_money >= $achievement->total_money &&
+                $user_friend >= $achievement->friend &&
+                $user_reply >= $achievement->reply &&
+                $user_post >= $achievement->post &&
+                $user_cloth >= $achievement->cloth &&
+                $user_throw_other_garbage >= $achievement->throw_other_garbage &&
+                $user_clean_self_garbage >= $achievement->clean_self_garbage &&
+                $user_other_throw_garbage >= $achievement->other_throw_garbage &&
+                $usre_clean_other_garbage >= $achievement->clean_other_garbage &&
+                $user_body_price >= $achievement->body_price )
+            {
+                $return[] = array('name' => $achievement->name, 'description' => $achievement->description);
+                // echo $achievement->name.'=>'.$achievement->description.'</br>';
+            }
+        }
+        return $return;
+    } 
+   
 }
--- application/views/game/achievements.php
+++ application/views/game/achievements.php
@@ -1,5 +1,13 @@
 ﻿<h1>目前使用者ID:<?php echo Yii::app()->user->getId()?> </h1>
 <h1>觀看使用者ID:<?php echo $user_id;?></h1>
+<?php
+// print_r($achievements);
+foreach($achievements as $achievement)
+{
+    echo $achievement['name'].' => '.$achievement['description'].'<br/>';
+}
+
+?>
 
 
 <img src="../statics/game/gamesystem.png">
