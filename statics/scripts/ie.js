83f3795a26ddafae2dc0b14ae91a6bfbe7d93b35
Sheng Han Yang <shenghan.yang@gmail.com>
2012-07-27 01:09:30 +0800

    [NEW] The new page for Internet Explorer 6-7 to get new browser. Note that the        PNG transparant problem is not solved now, and may add portable browsers.

--- statics/scripts/ie.js
+++ statics/scripts/ie.js
@@ -4,8 +4,102 @@
     {
         $('body *').hide();
         $('<div></div>')
-            .text('您的瀏覽器不被支援！')
-            .prependTo($('body'))
+            .attr('id', 'notsupport')
+            .append(
+                $('<ul></ul>')
+                    .append(
+                        $('<li></li>')
+                            .append(
+                                $('<a></a>')
+                                    .attr('href', 'http://www.microsoft.com.tw/ie')
+                                    .attr('title', 'Internet Explorer 9')
+                                    .text('Internet Explorer 9')
+                                    .prepend(
+                                        $('<span></span>')
+                                            .attr(
+                                                'id',
+                                                'notsupport-browser-ie9'
+                                            )
+                                    )
+                            )
+                    )
+                    .append(
+                        $('<li></li>')
+                            .append(
+                                $('<a></a>')
+                                    .attr('href', 'http://www.firefox.com')
+                                    .attr('title', 'Mozilla Firefox')
+                                    .text('Mozilla Firefox')
+                                    .prepend(
+                                        $('<span></span>')
+                                            .attr(
+                                                'id',
+                                                'notsupport-browser-firefox'
+                                            )
+                                    )
+                            )
+                    )
+                    .append(
+                        $('<li></li>')
+                            .append(
+                                $('<a></a>')
+                                    .attr('href', 'http://www.chrome.com')
+                                    .attr('title', 'Google  Chrome')
+                                    .text('Google  Chrome')
+                                    .prepend(
+                                        $('<span></span>')
+                                            .attr(
+                                                'id',
+                                                'notsupport-browser-chrome'
+                                            )
+                                    )
+                            )
+                    )
+                    .append(
+                        $('<li></li>')
+                            .append(
+                                $('<a></a>')
+                                    .attr('href', 'http://www.opera.com')
+                                    .attr('title', 'Opera')
+                                    .text('Opera')
+                                    .prepend(
+                                        $('<span></span>')
+                                            .attr(
+                                                'id',
+                                                'notsupport-browser-opera'
+                                            )
+                                    )
+                            )
+                    )
+                    .append(
+                        $('<li></li>')
+                            .append(
+                                $('<a></a>')
+                                    .attr('href', 'http://www.apple.com.tw/safari')
+                                    .attr('title', 'Apple Safari')
+                                    .text('Apple Safari')
+                                    .prepend(
+                                        $('<span></span>')
+                                            .attr(
+                                                'id',
+                                                'notsupport-browser-safari'
+                                            )
+                                    )
+                            )
+                    )
+            )
+            .appendTo($('body'))
             .show();
+        $('a').click(function()
+        {
+            window.open($(this).attr('href'));
+            return false;
+        });
+        for ( var index = 0, times = $.random(5, 30) ; index < times ; ++index)
+        {
+            $('#notsupport li')
+                .eq($.random(0, $('#notsupport li').length - 1))
+                .prependTo($('#notsupport ul'));
+        }
     });
 })(jQuery);
