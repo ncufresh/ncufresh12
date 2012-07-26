b12de394a76590f7a8080cb96136f59f6c86af53
Winston <win60615@yahoo.com.tw>
2012-07-26 16:50:12 +0800

    change view and midified script.js

--- statics/scripts/game.js
+++ statics/scripts/game.js
@@ -1,5 +1,7 @@
 (function($){
     $(document).ready(function(){
-        $('.game-display').scrollable();
+        $('.game-display').scrollable({
+            wheelSpeed: 90
+        });
     });
 })(jQuery);
