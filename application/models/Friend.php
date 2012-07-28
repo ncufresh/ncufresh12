ba4fe0c7d57553dae46b82bde41ccf4fa8d1375e
POPO <girl820628@hotmail.com>
2012-07-28 21:53:06 +0800

    manage my friend..

--- application/models/Friend.php
+++ application/models/Friend.php
@@ -30,82 +30,79 @@ class Friend extends CActiveRecord
         );
     }
     
-    public function closeFriend($userid,$friendid)
+    /*public function isFriend($userid,$friendid) //刪除好友...把自己的invisible變1
     {
         $userid = (integer)$userid;
         $friendid = (integer)$friendid;  
-        $temp = $this->updateAll(array(
+        $this->updateAll(array(
             'invisible' => 1
         ), "user_id = :userid AND friend_id = :friendid OR user_id = :friendid AND friend_id = :userid", array(
             ':userid' => $userid,
             ':friendid' => $friendid
         ));
-        if ( $temp == 2 )
-        {
-            return true;
-        }
-        return false;
-    }
-
-    public function openFriend($userid,$friendid)
+    }*/
+    public function isExist($userid,$friendid)
     {
-        $userid = (integer)$userid;
-        $friendid = (integer)$friendid;  
-        $temp = $this->updateAll(array(
-            'invisible' => 0
-        ), "user_id = :userid AND friend_id = :friendid OR user_id = :friendid AND friend_id = :userid", array(
-            ':userid' => $userid,
-            ':friendid' => $friendid
-        ));
-        /*if ( $temp == 2 )
-        {
-            return true;
-        }*/
-        return $temp;
-    }
-
-    public function isExist($userid,$friendid) //一次兩筆資料
-    {
-        $data1 = $this->find(array(
+        $data = $this->find(array(
             'condition' => 'user_id = :userid AND friend_id = :friendid',
             'params'    => array(
                 ':userid' => $userid,
                 ':friendid' => $friendid
             )
         ));
-        $data2 = $this->find(array(
-            'condition' => 'user_id = :userid AND friend_id = :friendid',
-            'params'    => array(
-                ':userid' => $friendid,
-                ':friendid' => $userid
-            )
-        ));
-        if ( isset($data1) && isset($data2) )
+        if ( $data )
         {
             return true;
         }
         return false;
     }
 
-    public function addFriend($user_id, $friend_id)
+    public function deleteFriend($userid,$friendid) //刪除好友關係
     {
-        $makefriend = false;
+        $userid = (integer)$userid;
+        $friendid = (integer)$friendid;  
+        $this->deleteAll(array(
+            'condition' => 'user_id = :userid AND friend_id = :friendid OR user_id = :friendid AND friend_id = :userid',
+            'params'    => array(
+                ':userid' => $userid,
+                ':friendid' => $friendid
+            )
+        ));
+    }
+    
+    public function makeFriend($userid,$friendid) //當兩筆資料都存在
+    {
+        $userid = (integer)$userid;
+        $friendid = (integer)$friendid;  
+        $data = $this->findAll(array(
+            'condition' => 'user_id = :userid AND friend_id = :friendid OR user_id = :friendid AND friend_id = :userid',
+            'params'    => array(
+                ':userid' => $userid,
+                ':friendid' => $friendid
+            )
+        ));
+        if ( count($data) === 2 )
+        {
+            $this->updateAll(array(
+                'invisible' => 0
+            ), "user_id = :userid AND friend_id = :friendid OR user_id = :friendid AND friend_id = :userid", array(
+                ':userid' => $userid,
+                ':friendid' => $friendid
+            ));
+        }
+    }
+
+    public function addFriend($user_id, $friend_id)// 一次新增一筆資料
+    {
         $model = new Friend();
         $model->user_id = $user_id;
         $model->friend_id = $friend_id;
-        $save1 = $model->save();
-        if ( isset($save1) )
+        $save = $model->save();
+        if ( isset($save) )
         {
-            $model = new Friend();
-            $model->user_id = $friend_id;
-            $model->friend_id = $user_id;
-            $save2 = $model->save();
-            if ( isset($save2) )
-            {
-                $makefriend = true;
-            }
+            return true;
         }
-        return $makefriend;
+        return false;
     }
 
 }
