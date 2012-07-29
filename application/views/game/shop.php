243e6cb0497f7c697d7c4ba120dc945786508d2c
Winston <win60615@yahoo.com.tw>
2012-07-29 16:14:21 +0800

    modified model and view

--- application/views/game/shop.php
+++ application/views/game/shop.php
@@ -1,1 +1,56 @@
-<h1> 商城列表 </h1>
+<h1> 可購買列表 </h1>
+<?php $array = Item::model()->getBuyItems(1, 1); 
+$counter=0;
+foreach($array as $row)
+{
+    $counter++;
+    echo $counter.'->'.$row->id.'->'.$row->name.'</br>';
+}?>
+</br>
+<?php $array = Item::model()->getBuyItems(1, 2); 
+$counter=0;
+foreach($array as $row)
+{
+    $counter++;
+    echo $counter.'->'.$row->id.'->'.$row->name.'</br>';
+}?>
+</br>
+<?php $array = Item::model()->getBuyItems(1, 3); 
+$counter=0;
+foreach($array as $row)
+{
+    $counter++;
+    echo $counter.'->'.$row->id.'->'.$row->name.'</br>';
+}?>
+</br>
+<?php $array = Item::model()->getBuyItems(1, 4); 
+$counter=0;
+foreach($array as $row)
+{
+    $counter++;
+    echo $counter.'->'.$row->id.'->'.$row->name.'</br>';
+}?>
+</br>
+<?php $array = Item::model()->getBuyItems(1, 5); 
+$counter=0;
+foreach($array as $row)
+{
+    $counter++;
+    echo $counter.'->'.$row->id.'->'.$row->name.'</br>';
+}?>
+</br>
+<?php $array = Item::model()->getBuyItems(1, 6); 
+$counter=0;
+foreach($array as $row)
+{
+    $counter++;
+    echo $counter.'->'.$row->id.'->'.$row->name.'</br>';
+}?>
+
+<?php $array = Item::model()->getBuyItems(1, 7); 
+$counter=0;
+foreach($array as $row)
+{
+    $counter++;
+    echo $counter.'->'.$row->id.'->'.$row->name.'</br>';
+}?>
