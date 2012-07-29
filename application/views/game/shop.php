aa419fb1ae78ff71b49786f4adaf4c7aad83c9de
Winston <win60615@yahoo.com.tw>
2012-07-29 21:51:11 +0800

    modified MVC

--- application/views/game/shop.php
+++ application/views/game/shop.php
@@ -1,10 +1,37 @@
+<style type="text/css">
+    div.game-shop
+    {
+        position: relative;
+    }
+    div.game-shop a
+    {
+        
+    }
+    div.game-shop:hover span
+    {
+        display: block;
+    }
+    div.game-shop span
+    {
+        position: absolute;
+        width: 200px;
+        height: 100px;
+        top: 0px;
+        left: -10px;
+        background: red;
+        display: none;
+    }
+</style>
 <h1> 可購買列表 </h1>
 <?php $array = Item::model()->getBuyItems(20, 1); 
 $counter=0;
 foreach($array as $row)
 {
     $counter++;
+    echo '<div class="game-shop">';
     echo '<a href="#'.$row->id.'">'.$counter.'->'.$row->id.'->'.$row->name.'->'.$row->price.'->LV.'.$row->level.'</br></a>';
+    echo '<span>'.$row->description.'</span>';
+    echo '</div>'; 
 }?>
 </br>
 <?php $array = Item::model()->getBuyItems(1, 2); 
