c8c5fa49a0e2c17658309f6066dc6e4ba11fd45f
Winston <win60615@yahoo.com.tw>
2012-07-31 15:09:51 +0800

    change M and V

--- application/views/game/missions.php
+++ application/views/game/missions.php
@@ -1,5 +1,5 @@
 <h1> 任務列表 </h1>
-<h1> 任務進度：<?php echo ($mission_count/91)*100 ?>% ( <?php echo $mission_count ?> / 91)</h1>
+<h1> 任務進度：<?php echo ($mission_count/93)*100 ?>% ( <?php echo $mission_count ?> / 91)</h1>
 <?php foreach ( $missions as $mission ) : ?>
 <h3> <?php echo $mission->name ?> </h3>
 <span> <?php echo $mission->content ?> </span>
