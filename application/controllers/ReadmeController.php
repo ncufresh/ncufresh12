ea8e886340c276c00546b2bb2edcc0d10133a61f
Sheng Han Yang <shenghan.yang@gmail.com>
2012-07-31 08:45:04 +0800

    [DEL] Removes useless actions in readme controller.

--- application/controllers/ReadmeController.php
+++ application/controllers/ReadmeController.php
@@ -7,29 +7,6 @@ class ReadmeController extends Controller
         $this->render('index');
     }
 
-    public function actionSchedule()
-    {
-        $schedule = new Schedule();
-        $data = $schedule->getId(1);
-        $this->render('schedule', array(
-            'model'     => $data
-        ));
-    }
-
-    public function actionUpdate()
-    {
-        if ( Yii::app()->request->getIsPostRequest() )
-        {
-            $model = Schedule::model()->findByPk(1);
-            $model->status = ! $model->status;
-
-            if ( $model->validate() && $model->save() )
-            {
-                $this->redirect(array('readme/schedule'));
-            }
-        }
-    }
-
     public function actionFreshman()
     {
         $this->render('freshman');
