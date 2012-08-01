1a9fa04c973fff1d01525c467d30ab70e6d07108
sin <xxice520@hotmail.com>
2012-08-01 16:35:05 +0800

    [Fix] readmecontroller

--- application/views/readme/notice.php
+++ application/views/readme/notice.php
@@ -1,12 +1,4 @@
 <div id="readme-notice">
     <div id="readme-view"></div>
     <a href="<?php echo Yii::app()->createUrl('readme/index'); ?>" id="notice-back" class="readme-back"></a>
-    <div id="read-notice-menu" class="readme-menu">
-        <p>相關事項</p>
-        <ul id="readme-index3" class="readme-menu-index">
-<?php for ( $x = 0 ; $x < $size ; ++$x ) : ?>
-            <li tab="<?php echo $tab[$x]; ?>" page="notice"><?php echo $index[$x]; ?></li>
-<?php endfor; ?>
-        </ul>
-    </div>
 </div>
