ea83dce9de570d030abae17b549a204c548f2560
sin <xxice520@hotmail.com>
2012-08-01 20:02:42 +0800

    [New] readme-text

---
+++ application/views/readme/reschool/8.php
@@ -0,0 +1,38 @@
+<h1>
+繳交學雜費</h1>
+<p>
+(一)<strong>繳費單</strong>於<strong>8月</strong><strong>20</strong><strong>日</strong>由<u>出納組</u>郵寄，僑生及外籍生請自行上Portal列印繳費單。亦可直接至<a href="https://eschool.firstbank.com.tw/">第一銀行第e學雜費入口網</a>之代收學雜費專區或本校首頁Portal入口之「學生繳費單」中，下載繳費單，或以電話(分機：57346)與<u>出納組</u>聯繫。</p>
+<p>
+(二)<strong>繳費手續</strong>請在<strong>9月14日(含)前</strong>於<u>第一銀行</u>全省各分行臨櫃繳款。以提款機轉帳、信用卡或超商繳款者，於9月16日前完成<strong> (就學貸款申辦期限比照辦理，並請配合臺灣銀行作業時間洽辦)。</strong></p>
+<p>
+(三)延遲繳費者，應辦理註冊假，延遲註冊最遲至<strong>9月28日</strong>，否則依學則第9條之規定予以退學處分。</p>
+<p>
+(四)修習教育學程者在完成加退選後，請於<strong>10月15日至26日</strong>繳交學分費。逾期未繳交學分費者，<strong>選修學分一律取消</strong>，學士班學生如因此而致學分不足者，依本校學則第11及51條之規定，<strong>應予退學</strong>(繳費單由出納組送發各系所辦公室)。</p>
+<p>
+(五)申請就學貸款者：</p>
+<p>
+1.取得繳費單後<strong>(研究生及大學部延畢生請先至「就學補助系統」完成「學分預估」)</strong>，至<a href="https://sloan.bot.com.tw/sloan/sLoanLogin.do">台灣銀行網站</a>申請<strong>及登錄就學</strong>貸款<strong>，再至各縣市臺灣銀行完成手續</strong>。</p>
+<p>
+2.至本校教務處「<a href="http://www4.is.ncu.edu.tw/register/student.php">學籍暨成績查詢系統</a>」務必填寫「郵局局帳號、手機號碼」，以便將多貸的款項退費至帳戶，或手續未完成時之緊急通聯。</p>
+<p>
+3.於<strong>9月17~21日前</strong>將臺灣銀行「撥貸通知書第二聯」送繳至學務處生活輔導組，即完成就貸手續。(為縮短辦理註冊時間及流程，凡註冊週前已提早辦畢就學貸款手續者，可先行將就貸撥款通知書第二聯以掛號信郵寄至本校學務處生輔組吳先生收)。逾期未繳件者，視同未申辦就學貸款，屆時將通知補繳學費。</p>
+<p>
+4.就學貸款相關規定請至大一生活知訊網參考<strong>或「就學補助系統」相關公告</strong>。</p>
+<p>
+(六)申請<strong>學雜費減免</strong>(休學前當學期已申請減免費用，不得重複減免)最遲請於<strong>8月2日至9日</strong>前將表格(請自行於學雜費減免系統下載，網址：<strong><a href="http://www.ncu.edu.tw/">本校首頁</a></strong>&rarr;<a href="https://portal.ncu.edu.tw/wps/portal/!ut/p/cxml/04_Sj9SPykssy0xPLMnMz0vM0Q_wyU_PzNMvSFdUBACQIYPZ"><strong>Porta入口</strong></a>(輸入ID及密碼)&rarr;<strong>就學補助系統</strong>&rarr;<strong>學雜費減免申請</strong>&rarr;<strong>登錄申請所需資料</strong>；或<a href="http://ncufresh.ncu.edu.tw/">大一生活知訊網</a>中下載)填妥，連同減免申請資料寄至學務處生活輔導組。如未能及時辦理者，請於郵寄後一週自行上網列印繳費單後再行繳費。</p>
+<p>
+(七)弱勢學生助學計畫助學金(年收入70萬以下)請於<strong>9月17日起至10月12日止</strong>到<strong><a href="http://www.ncu.edu.tw/">本校首頁</a></strong>&rarr;<a href="https://portal.ncu.edu.tw/wps/portal/!ut/p/cxml/04_Sj9SPykssy0xPLMnMz0vM0Q_wyU_PzNMvSFdUBACQIYPZ"><strong>Portal入口</strong></a>(輸入ID及密碼)&rarr;<strong>就學補助系統</strong>&rarr;<strong>弱勢助學</strong>&rarr;<strong>登錄申請所需資料</strong>，並繳交應繳證件至生輔組辦理<strong>，</strong>同一學年已領有助學金者<strong>，</strong>不得重複申請。</p>
+<p>
+(八)<strong>繳費單收據(或提款機轉帳明細單)</strong>請妥為保存，以便日後核對有疑義時作為個人繳費證明或退費依據，及作為次年申報所得稅憑單。</p>
+<p>
+(九)繳費單相關問題請洽出納組(分機：57346)，就學貸款、<strong>弱勢學生工讀、</strong>各項減免及優待等事宜請洽詢<strong>生活輔導組</strong>(分機：57221~4)，住宿及押金問題請洽生活輔導組宿舍服務中心(分機：57290、57282)。</p>
+<p>
+(十)學生團體保險拒保者，請於<strong>9月21日前</strong>至<a href="http://health.ncu.edu.tw">衛生保健組網站</a>文件下載區下載「學生團體保險棄保切結書」，列印並填寫資料，送交衛保組呂小姐(分機：57270)辦理。</p>
+<p>
+(十一)<strong>休、退學退費標準：</strong></p>
+<p>
+<strong>1.9</strong><strong>月17日(含)申請休學者，全額退費(學生團體保險費除外)。</strong></p>
+<p>
+<strong>2. 9</strong><strong>月18日至10月26日退2/3，10月29日至12月7日退1/3，12月10日以後，所繳各費不予退還。</strong></p>
+<p>
+(十二) <strong>欲申請生活助學金工讀者，請逕向學務處生活輔導組申辦。</strong></p>
