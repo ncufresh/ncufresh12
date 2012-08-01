e53e13becc1b95c1000172924a2c6b3374372fbc
POPO <girl820628@hotmail.com>
2012-08-01 11:44:31 +0800

    modify css editor controller

--- statics/scripts/site.js
+++ statics/scripts/site.js
@@ -1,6 +1,5 @@
 (function($){
     $(document).ready(function(){
-<<<<<<< HEAD
         var daysInMonth = function(iYear, iMonth)
         {
             return 32 - new Date(iYear, iMonth-1, 32).getDate();
@@ -13,8 +12,6 @@
                 $('<option></option>').text(i).appendTo($("select.day")).val(i);
             }
         });
-=======
         $('#register > form').scrollable();
->>>>>>> 992d9414b0888f5fee76fd33cad9b3a075203723
     });
 })(jQuery);
