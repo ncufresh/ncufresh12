1a0a63d6142fe57cc4be8264cbf5a02e9191c77d
sin <xxice520@hotmail.com>
2012-08-01 14:55:36 +0800

    [New] picture-nculife

---
+++ application/views/nculife/live/14.php
@@ -0,0 +1,16 @@
+<h1>
+光南</h1>
+<p>
+光南大批發－中壢店</p>
+<p>
+除了在校園內的敦煌書局，可以買到上課文具品外，在市區的光南更是不錯的選擇，另外還賣各式各樣生活百貨、CD等，價格優惠、選擇多元化，是大學生常去的店家之一。</p>
+<p>
+中壢店聯絡電話：(03)426-9636</p>
+<p>
+中壢店地址：中壢市中山路100號</p>
+<p>
+<span>營業時間：10：00~22 ：00</span></p>
+<p>
+<img alt="光南" src="<?php echo Yii::app()->request->baseUrl; ?>/statics/documents/DSC_0312.JPG" style="width:500px;height:332px;border-width:2px;border-style:solid;margin:5px;" /></p>
+<p>
+<img alt="光南" src="<?php echo Yii::app()->request->baseUrl; ?>/statics/documents/DSC_0316.JPG" style="width:500px;height:332px;border-width:2px;border-style:solid;margin:5px;" /></p>
