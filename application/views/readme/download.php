607564ce4ad37fb832376293b8dcdc032e7f292d
sin <xxice520@hotmail.com>
2012-08-02 18:29:18 +0800

    [Fix] readme-download

--- application/views/readme/download.php
+++ application/views/readme/download.php
@@ -17,18 +17,17 @@
         <ul class="download-index">
         	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/附件-校區平面圖.pdf" title="附件-校區平面圖">附件-校區平面圖</a></li>
         	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/附件-路線示意圖.jpg" title="附件-路線示意圖">附件-路線示意圖</a></li>
-            <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/校園性侵害或性騷擾防治規定.doc" title="校園性侵害或性騷擾防治規定">校園性侵害或性騷擾防治規定</a></li>
-        	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/學士班新生註冊須知.doc" title="學士班新生註冊須知">學士班新生註冊須知</a></li>
-        	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/李代理校長給新生的話20120719.doc" title="李代理校長給新生的話">李代理校長給新生的話</a></li>
+        	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/大一國文.pdf" title="大一國文">大一國文</a></li>
+        	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/學習導航手冊.doc" title="學習導航手冊">學習導航手冊</a></li>
         	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/學分抵免申請表.doc" title="學分抵免申請表">學分抵免申請表</a></li>
         	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/役男兵役資料表.doc" title="役男兵役資料表">役男兵役資料表</a></li>
+        	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/學士班新生註冊須知.doc" title="學士班新生註冊須知">學士班新生註冊須知</a></li>
+        	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/李代理校長給新生的話20120719.doc" title="李代理校長給新生的話">李代理校長給新生的話</a></li>
+            <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/校園性侵害或性騷擾防治規定.doc" title="校園性侵害或性騷擾防治規定">校園性侵害或性騷擾防治規定</a></li>
         	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/附件-大一新生取消住宿申請單.doc" title="大一新生取消住宿申請單">大一新生取消住宿申請單</a></li>
         	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/生活助學金申請表.doc" title="生活助學金申請表">生活助學金申請表</a></li>
-        	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/學習導航手冊.doc" title="學習導航手冊">學習導航手冊</a></li>
-        	<li><a href="<?php echo Yii::app()->request->baseUrl; ?>/files/documents/大一國文.pdf" title="大一國文">大一國文</a></li>
         </ul>
     </div>
     <a href="<?php echo Yii::app()->createUrl('readme/index'); ?>" id="download-back" class="readme-back" title="回上一頁"></a>
-    <h4>文件下載</h4>
     <a href="<?php echo Yii::app()->createUrl('readme/index'); ?>" id="download-back" class="readme-back"></a>
 </div>
