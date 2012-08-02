7667ebb7ed8ada491020f94181c84fd41dac4785
DiMo <a968574123@hotmail.com>
2012-08-02 23:39:50 +0800

    [NEW] Modified style of subscription page.

--- application/views/calendar/subscript.php
+++ application/views/calendar/subscript.php
@@ -1,22 +1,40 @@
-﻿<div  id="calendar-subscript">
+﻿<?php $this->beginWidget('system.web.widgets.CClipWidget', array('id' => 'script')); ?>
+<script type="text/javascript">
+jQuery(document).ready(function()
+{
+    $('.category-button').click(function()
+    {
+        var id = $(this).attr('href').replace('#','');
+        $('.calendar-subscript-category').hide();
+        $('#calendar-subscript-category-' + id).show();
+    });
+    $('.category-button').first().click();
+    $('.calendar-subscript-container').scrollable();
+});
+</script>
+<?php $this->endWidget();?>
+<div id="calendar-subscript">
     <h4>訂閱</h4>
+    <div class="category-wrapper">
 <?php foreach($result as $key => $clubs): ?>
-    <a href="#<?php echo $key?>"><?php echo Club::getCategoryName($key); ?></a>
+        <a class="category-button" href="#<?php echo $key?>"><?php echo Club::getCategoryName($key); ?></a>
 <?php endforeach; ?>
+    </div>
     <form name="calendar-subscript" action="<?php echo Yii::app()->createUrl('calendar/subscript'); ?>" method="POST">
-        
+        <div class="calendar-subscript-container">
 <?php foreach($result as $key => $clubs): ?>
-        <div id="calendar-subscript-category-<?php echo $key?>" class="calendar-subscript-category" style="width: auto;">
+            <div id="calendar-subscript-category-<?php echo $key?>" class="calendar-subscript-category" style="width: auto;">
 <?php foreach($clubs as $club): ?>
-        <dl>
-            <dt><label for="form-subscript-<?php echo $club->club->id; ?>"><?php echo $club->club->name; ?></label></dt>
-            <dd>
-                <input id="form-subscript-<?php echo $club->club->id; ?>" type="checkbox" name="subscript[<?php echo $club->id; ?>]" value="1" <?php if( isset($club->subscriptions->invisible) && $club->subscriptions->invisible == 0) echo 'checked'; ?>/> 
-            </dd>
-        </dl>
+            <dl>
+                <dt><label for="form-subscript-<?php echo $club->club->id; ?>"><?php echo $club->club->name; ?></label></dt>
+                <dd>
+                    <input id="form-subscript-<?php echo $club->club->id; ?>" type="checkbox" name="subscript[<?php echo $club->id; ?>]" value="1" <?php if( isset($club->subscriptions->invisible) && $club->subscriptions->invisible == 0) echo 'checked'; ?>/> 
+                </dd>
+            </dl>
+<?php endforeach; ?>
+            </div>
 <?php endforeach; ?>
         </div>
-<?php endforeach; ?>
         <button type="submit" id="subscript-submit">確定</button>
         <input type="hidden" name="token" value="<?php echo Yii::app()->security->getToken(); ?>" />
     </form>
--- application/views/site/register.php
+++ application/views/site/register.php
@@ -141,7 +141,7 @@
             <input id="form-register-birthday" name="profile[birthday]" type="text" />
         </dd>
     </dl>
-    <div>
+    <div class="buttons">
         <input name="token" value="<?php echo Yii::app()->security->getToken(); ?>" type="hidden" />
         <button type="submit">註冊</button>
         <button type="reset">重填</button>
