429153e0b682c952c75e6cd68435b66cd45947fc
sin <xxice520@hotmail.com>
2012-08-02 12:56:37 +0800

    [Add] document-readme notice [Fix] readmeController

---
+++ application/views/readme/notice/10.php
@@ -0,0 +1,7 @@
+<h1>
+種子、志工團隊</h1>
+<p>
+<img alt="種子、志工團隊" src="<?php echo Yii::app()->request->baseUrl; ?>/statics/documents/%E7%A8%AE%E5%AD%90%E5%BF%97%E5%B7%A5.jpg" style="width:680px;height:340px;" /></p>
+<p>
+<a href="http://service-learning.ncu.edu.tw/">中大服務學習網</a></p>
+
