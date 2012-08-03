a25e1f5bb454cedc71c01d86d68608d5d5f17358
sin <xxice520@hotmail.com>
2012-08-03 11:28:58 +0800

    [Fix] style in readme\nculife

--- application/views/readme/notice.php
+++ application/views/readme/notice.php
@@ -1,9 +1,9 @@
-<div id="readme-notice">
+<div id="readme-notice" class="readme-background">
     <div class="readme-view"></div>
-    <a href="<?php echo Yii::app()->createUrl('readme/index'); ?>" id="notice-back" class="readme-back"></a>
+    <a href="<?php echo Yii::app()->createUrl('readme/index'); ?>" id="notice-back" class="readme-back" title="回上一頁"></a>
     <div id="read-notice-menu" class="readme-menu">
         <p>相關須知</p>
-        <ul id="readme-index3" class="readme-menu-index">
+        <ul class="readme-menu-index">
 <?php for ( $x = 0 ; $x < $size ; ++$x ) : ?>
             <li tab="<?php echo $tab[$x]; ?>" page="notice"><?php echo $index[$x]; ?></li>
 <?php endfor; ?>
