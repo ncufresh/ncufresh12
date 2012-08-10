5da515e28816e081ff1e575f6554663b385bc364
DiMo <a968574123@hotmail.com>
2012-08-10 16:57:14 +0800

    [MOD] Modified some documents of street.

--- application/views/street/department-building/content6.php
+++ application/views/street/department-building/content6.php
@@ -23,5 +23,14 @@
     科系：<a href="http://www.ncu.edu.tw/~ls/" title="生科">生科</a>
 </p>
 <p>
-    <img alt="科學五館" src="<?php echo Yii::app()->request->baseUrl; ?>/statics/documents/IMG_2794.JPG" style="width:320px;height:180px;" /><img alt="科學五館" src="<?php echo Yii::app()->request->baseUrl; ?>/statics/documents/IMG_2792.JPG" style="width:320px;height:180px;" />
-</p>
+    <img alt="科學五館" src="<?php echo Yii::app()->request->baseUrl; ?>/statics/documents/IMG_2794.JPG" style="width:320px;height:180px;" />
+</p>
+<h5>
+	公共藝術-DNA交響曲
+</h5>
+<p>
+	為了展示生命科學學科的研究動向，即科學五館師生所投入的活動成果，楊奉琛以DNA纏繞而上的雙螺旋體為作品的發想概念。《DNA交響曲》以不鏽鋼材料創作虛實交錯的螺旋體造型，營造出富有律動性與豐富變化的視覺效果，其材質表面會隨著光線產生變化。
+</p>
+<p>
+    <img alt="科學五館" src="<?php echo Yii::app()->request->baseUrl; ?>/statics/documents/IMG_2792.JPG" style="width:320px;height:180px;" />
+</p>
